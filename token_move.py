e9f1285e47ba210e48e82cbca035a5ca14e58308
alexpsimone <alex.p.simone@gmail.com>
2021-01-26 19:22:51 +0000

    utilized is_valid_move_red twice

--- token_move.py
+++ token_move.py
@@ -34,17 +34,14 @@ def token_move_final_red(move_options, red_score, player1_board):
                 user_selec_is_higher = [1 for i in range(0, len(move_options)) if token_pos > move_options[i]]
 
                 if len(user_selec_is_higher) == len(move_options):
+                    
                     token_new_pos = int(input("Select new position for this token: "))
-                    if token_new_pos > token_pos:
-                        print("You can't move your token in that direction! Try again!")
-                    elif token_new_pos < 1:
-                        print("You can't move your token off the board like that! Try again!")
-                    elif (token_pos - token_new_pos) in move_options:
-                        player1_board[token_pos - 1] -= 1
-                        player1_board[token_new_pos - 1] += 1
-                        move_options.remove(token_pos - token_new_pos)
-                    else:
-                        print("You have to move a token based on the dice roll values! Try again!")
+
+                    if is_valid_move_red(token_pos, token_new_pos, move_options):
+                            player1_board[token_pos - 1] -= 1
+                            player1_board[token_new_pos - 1] += 1
+                            move_options.remove(token_pos - token_new_pos)
+
                 elif len(user_selec_is_higher) == 0:
                     tokens_in_higher_pos = [1 for i in range(token_pos, 6) if player1_board[i] > 1]
                     if len(tokens_in_higher_pos) > 0:
